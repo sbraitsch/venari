e8c5adc75357854493d46fafd2e58942d901a618
Simon Braitsch <S.Braitsch@gmx.de>
2022-07-24 14:54:06 +0200

    add cors handling

--- venari/src/main/kotlin/dev/sbraitsch/venari/security/VenariSecurityConfiguration.kt
+++ venari/src/main/kotlin/dev/sbraitsch/venari/security/VenariSecurityConfiguration.kt
@@ -4,8 +4,10 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 
 @Configuration
 class VenariSecurityConfiguration {
@@ -16,5 +18,15 @@ class VenariSecurityConfiguration {
                 auth -> auth.mvcMatchers("/v1/**").permitAll() }
             .httpBasic(Customizer.withDefaults())
         return http.build()
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = listOf("https://sbraitsch.dev")
+        configuration.allowedMethods = listOf("GET", "POST")
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
     }
 }
