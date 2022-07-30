7fba65bb7e7a24d2a6e2aa668833c2a073699a16
sbr <S.Braitsch@gmx.de>
2022-07-30 17:22:08 +0000

    deploy.sh fix

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
-cd /home/admin/projects/venari
-git pull
-cd venari-ui
-npm install
-npm run build
-rm -r /var/www/html/venari/*
-mv -v dist/* /var/www/html/venari
+cd /home/admin/projects/venari &&
+git pull &&
+cd venari-ui &&
+npm install &&
+npm run build &&
+sudo rm -r /var/www/html/venari/*
+sudo mv -v dist/* /var/www/html/venari
